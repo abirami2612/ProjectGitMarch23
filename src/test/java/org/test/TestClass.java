b826b01e88c8a1e9c8890de1da9656000bd5020c
abirami2612 <abirami232@gmail.com>
2023-03-23 16:03:07 +0530

    project structures

---
+++ src/test/java/org/test/TestClass.java
@@ -0,0 +1,13 @@
+package org.test;
+
+import org.base.BaseClass;
+import org.openqa.selenium.support.PageFactory;
+
+public class TestClass extends BaseClass{
+	
+	public TestClass() {
+		PageFactory.initElements(driver, this);
+	}
+	
+	
+}
